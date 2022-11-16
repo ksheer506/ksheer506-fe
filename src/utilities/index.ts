18838fcaea1b9007fe270c3333e9fc64f7695dab
ksheer506 <ksheer506@naver.com>
2022-11-17 00:59:26 +0900

    feat: formatPrice 유틸 함수 추가

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,13 +1,2 @@
-export const parseQueryString = (search: string): Record<string, string> =>
-  (search || '')
-    .replace(/^\?/g, '')
-    .split('&')
-    .reduce((acc, query) => {
-      const [key, value] = query.split('=');
-
-      if (key) {
-        acc[key] = decodeURIComponent(value);
-      }
-
-      return acc;
-    }, {} as Record<string, string>);
+export { formatPrice } from './formatPrice';
+export { parseQueryString } from './parseQueryString';
