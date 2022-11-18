2d12f1ed38439ab46a409c30e6efac5d1c185dca
ksheer506 <ksheer506@naver.com>
2022-11-18 18:52:00 +0900

    chore: components 폴더에 index.ts 추가

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { ProductItemSkeleton } from './ProductItemSkeleton';
+export { Nav } from './Nav';
+export { default as Pagination } from './Pagination';
+export { default as ProductItem } from './ProductItem';
+export { default as ProductList } from './ProductList';
+export { Rectangle, Skeleton } from './Skeleton/Skeleton';
