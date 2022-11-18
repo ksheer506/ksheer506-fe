311e011eb721314358f3a62dffa11af6f901712c
ksheer506 <ksheer506@naver.com>
2022-11-18 18:08:21 +0900

    #3 feat: Product 타입 추가

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,6 +1,18 @@
-export type Product = {
+export interface Product {
   id: string;
   name: string;
   thumbnail: string | null;
   price: number;
+}
+
+export interface ProductListPayload {
+  page: number;
+  size: number;
+}
+
+export interface ProductListResponse {
+  data: {
+    products: Product[];
+    totalCount: number;
+  };
 }
