163959111bb115bc3aa050cb4789aeece238f0fc
ksheer506 <ksheer506@naver.com>
2022-11-18 22:07:15 +0900

    chore: ProductList 시멘틱 수정

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,22 +2,23 @@ import styled from 'styled-components';
 
 import { Product } from '../types/product';
 import ProductItem from './ProductItem';
+import { ProductItemSkeleton } from './ProductItemSkeleton';
 
 type ProductListProps = {
   products: Product[];
 };
 
 const ProductList = ({ products }: ProductListProps) => (
-  <Container>
+  <UList>
     {products.map((product) => (
-      <ProductItem key={product.id} product={product} />
+      <ProductItem product={product} skeleton={<ProductItemSkeleton />} key={product.id} />
     ))}
-  </Container>
+  </UList>
 );
 
 export default ProductList;
 
-const Container = styled.ul`
+const UList = styled.ul`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   width: 400px;
