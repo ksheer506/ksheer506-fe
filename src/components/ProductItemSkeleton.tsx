6226f890547824ce59f0e51f6c0d91a72cb389a7
ksheer506 <ksheer506@naver.com>
2022-11-19 11:30:11 +0900

    fix: <li> cannot appear as a descendant of <li> 수정

--- src/components/ProductItemSkeleton.tsx
+++ src/components/ProductItemSkeleton.tsx
@@ -3,22 +3,14 @@ import styled from 'styled-components';
 import { Rectangle, Skeleton } from './Skeleton/Skeleton';
 
 export const ProductItemSkeleton = () => (
-  <List>
-    <Skeleton width='180px' height='230px'>
-      <Container>
-        <Rectangle width='100%' height='180px' />
-        <Rectangle width='100%' height='20px' />
-        <Rectangle width='45%' height='20px' />
-      </Container>
-    </Skeleton>
-  </List>
+  <Skeleton width='180px' height='230px'>
+    <Container>
+      <Rectangle width='100%' height='180px' />
+      <Rectangle width='100%' height='20px' />
+      <Rectangle width='45%' height='20px' />
+    </Container>
+  </Skeleton>
 );
-
-const List = styled.li`
-  width: 180px;
-  align-self: center;
-  justify-self: center;
-`;
 
 const Container = styled.div`
   display: flex;
