7de55d20a5f288cfad472f5edac2075fa6d53dfa
ksheer506 <ksheer506@naver.com>
2022-11-21 15:32:04 +0900

    #1 feat: 유저 정보 없을 경우 Store 데이터 초기화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,25 @@
 import Link from 'next/link';
-import type { GetServerSideProps, NextPage } from 'next';
+import type { NextPage } from 'next';
 import React from 'react';
 import styled from 'styled-components';
 import { useQuery } from 'react-query';
-import { selectUserInfos, useAppSelector } from '../redux';
+import { logOutUser, selectUserInfos, useAppDispatch, useAppSelector } from '../redux';
 import { Nav } from '../components/Nav';
 import { queryUserInfos } from '../api';
 import { toast } from 'react-toastify';
-import { AxiosResponse } from 'axios';
 import { UserInfos, UserInfosResponse } from '../types';
 
 type HomPageProps = Pick<UserInfos, 'NAME'>;
 
-const HomePage: NextPage<HomPageProps> = ({ NAME }) => {
+const HomePage: NextPage<HomPageProps> = () => {
   const { ID } = useAppSelector(selectUserInfos);
+  const dispatch = useAppDispatch();
   const { data } = useQuery<UserInfosResponse>(['user', ID], () => queryUserInfos({ ID }), {
     retry: 1,
     enabled: !!ID,
     onError: (err) => {
       toast.error('로그인 상태를 확인해주세요.');
+      dispatch(logOutUser());
     },
   });
 
@@ -37,24 +38,6 @@ const HomePage: NextPage<HomPageProps> = ({ NAME }) => {
   );
 };
 
-/* export const getServerSideProps: GetServerSideProps<{
-  NAME: string;
-}> = async ({ req }) => {
-   const currentUser: UserInfos | undefined = JSON.parse(
-    localStorage.getItem('currentUser') || '""'
-  );
-  if (!currentUser) return { props: { NAME: '' } };
-
-  const { ID, NAME } = { ID: 'sixshop_001', NAME: 'Fg' };
-  const { data } = await fetchUserInfos({ ID });
-
-  if (data) {
-    return { props: { NAME } };
-  }
-  return {
-    notFound: true, // will be passed to the page component as props
-  };
-}; */
 
 export default HomePage;
 
