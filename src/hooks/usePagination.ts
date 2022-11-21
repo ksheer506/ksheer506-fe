c7c5a186197a44b859f1a0dbec55b3c747fe4242
ksheer506 <ksheer506@naver.com>
2022-11-21 12:46:25 +0900

    #3 chore: generatePages 로직 수정

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -7,8 +7,13 @@ function generatePages(currentPage: number, size: number, lastPage?: number) {
   const start = (Math.ceil(currentPage / size) - 1) * size + 1;
   const end = Math.ceil(currentPage / size) * size;
 
-  if (lastPage && end > lastPage) {
+  if (!lastPage) return [];
+  if (end > lastPage && start <= lastPage) {
     return new Array(lastPage - start + 1).fill(0).map((_, i) => start + i);
+  }
+
+  if (start > lastPage) {
+    return [lastPage];
   }
 
   return new Array(size).fill(0).map((_, i) => start + i);
@@ -23,7 +28,7 @@ interface usePaginationProps {
 
 export const usePagination = ({ currentPage, size, lastPage, onChange }: usePaginationProps) => {
   const initialPage = useRef<number | undefined>(currentPage);
-  const [pages, setPages] = useState<number[]>(generatePages(currentPage || 1, size));
+  const [pages, setPages] = useState<number[]>(generatePages(currentPage || 1, size, lastPage));
 
   // FIXME: pagination 클릭해서 들어온 후 다시 주소창 입력해서 페이지 전환할 경우 lastPage가 제대로 적용되지 않음
   // 주소창에서 직접 페이지를 변경했을 때 해당 페이지가 표시되지 않는 문제 처리
@@ -31,7 +36,7 @@ export const usePagination = ({ currentPage, size, lastPage, onChange }: usePagi
     const { current } = initialPage;
 
     if (current || !currentPage) return;
-    
+
     setPages(generatePages(currentPage, size, lastPage));
   }, [currentPage, lastPage, size]);
 
